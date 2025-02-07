56e72064abff6eab4c4d97c29ab379e626f9edc3
penguinite <147942000+penguinite@users.noreply.github.com>
2025-02-07 10:04:05 +0100

    feat: Update rescan.nim

--- src/rescan.nim
+++ src/rescan.nim
@@ -4,24 +4,13 @@ import fp/[utils], std/[os, tables]
 
 var versionData = parseNimData(getNimDataFile())
 
-when compileOption("threads"):
-  if not fileExists("bin/rescan_cmd"):
-    discard execShellCmd("nimble build rescan_cmd")
-
-# The easiest way to do multi-threading is to just utilize commands
-# Anyway this is the max number of commands we will run per minute
+# This is the max number of commands we will run per minute
 # I just chose 8 arbitrarily but you can set it to anything with -d:limit=NUMBER
 const limit{.intdefine.} = 8
 var num = 0 # Number of *current* tasks running.
 for hash in versionData[0].keys:
-  when compileOption("threads"):
-    echo "Threads enabled! Running ", limit, " rescans at a time!"
-    if num >= limit:
-      sleep(60000)
-      num = 0
-    inc num
-    discard execShellCmd("bin/rescan_cmd " & hash)
-  else:
-    echo "Threads disabled... This will be way slower than usual."
-    echo "Consider compilling with --threads:off to get a significant performance boost."
-    rescanVersion(hash, versionData)
+  inc num
+  if num >= limit:
+    sleep(60000)
+    num = 0
+  rescanVersion(hash, versionData)
