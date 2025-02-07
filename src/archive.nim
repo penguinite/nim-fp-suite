1a1a4dcd07d1c722e28a30235e6db0b6e7044565
penguinite <penguinite@tuta.io>
2025-02-07 13:19:15 +0100

    refactor: Remove line

--- src/archive.nim
+++ src/archive.nim
@@ -3,7 +3,6 @@ if not dirExists("json"):
   echo "No json folder to archive, run \"nimble run fetch\" first in order to fetch data."
   quit(1)
 
-
 # Create the archive folder
 createDir("archive")
 
