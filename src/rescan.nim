4be96e72f15316f08080fabe2688fab68087b524
penguinite <penguinite@tuta.io>
2025-02-07 20:36:28 +0100

    refactor: The limit of 8 is too high maybe and it would set off VT's ratelimit

--- src/rescan.nim
+++ src/rescan.nim
@@ -10,7 +10,7 @@ var versionData = parseNimData(getNimDataFile())
 
 # This is the max number of commands we will run per minute
 # I just chose 8 arbitrarily but you can set it to anything with -d:limit=NUMBER
-const limit{.intdefine.} = 8
+const limit{.intdefine.} = 5
 var num = 0 # Number of *current* tasks running.
 for hash in versionData[0].keys:
   inc num
