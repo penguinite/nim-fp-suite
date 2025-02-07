9ed1c1c9d5767c146bd5c9810ecc0981919cd14d
penguinite <penguinite@tuta.io>
2025-02-07 13:19:04 +0100

    feat: Improve "all" command

--- src/all.nim
+++ src/all.nim
@@ -1,5 +1,8 @@
-import std/osproc
+import std/[osproc, os]
 
 discard execCmd("nimble run rescan")
+echo "Waiting an hour for the rescans to finish"
+sleep(3600000)
 discard execCmd("nimble run fetch")
 discard execCmd("nimble run generate")
+discard execCmd("nimble run archive")
